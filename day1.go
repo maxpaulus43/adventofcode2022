b7a4be69592ee6088f29f73adfb973aa6a5ddff5
Max Paulus 🥪 <maxpaulu@amazon.com>
2022-12-01 07:42:26 -0800

    day1

--- day1.go
+++ day1.go
@@ -1,5 +1,48 @@
 package main
 
-func day1Part1() string {
-	return "hello day 1"
+import (
+	"sort"
+	"strconv"
+)
+
+func day1Part1() int {
+	input := linesFromFile("inputs/day1.txt")
+	mostCalories := 0
+	currElfCalories := 0
+
+	for _, cal := range input {
+		if len(cal) == 0 {
+			currElfCalories = 0
+		} else {
+			c, err := strconv.Atoi(cal)
+			check(err)
+			currElfCalories += c
+			if currElfCalories > mostCalories {
+				mostCalories = currElfCalories
+			}
+		}
+	}
+
+	return mostCalories
+}
+
+func day1Part2() int {
+	input := linesFromFile("inputs/day1.txt")
+	caloriesPerElf := make([]int, 0)
+
+	currElfCalories := 0
+	for _, cal := range input {
+		if len(cal) == 0 {
+			caloriesPerElf = append(caloriesPerElf, currElfCalories)
+			currElfCalories = 0
+		} else {
+			c, err := strconv.Atoi(cal)
+			check(err)
+			currElfCalories += c
+		}
+	}
+
+	sort.Ints(caloriesPerElf)
+
+	return sum(caloriesPerElf[len(caloriesPerElf)-3:]...)
 }
--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(day1Part1())
+	fmt.Println(day1Part2())
 }
