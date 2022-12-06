1bf032f926d96d77176c2aa0fa14601ebb08e7cb
Max Paulus 🥪 <maxpaulu@amazon.com>
2022-12-05 21:10:39 -0800

    day6

---
+++ day6.go
@@ -0,0 +1,23 @@
+package main
+
+func day6Part1() int {
+	line := stringFromFile("inputs/day6.txt")
+	for i := 0; i < len(line)-4; i++ {
+		set := runeSet(line[i : i+4])
+		if len(set) == 4 {
+			return i + 4
+		}
+	}
+	return -1
+}
+
+func day6Part2() int {
+	line := stringFromFile("inputs/day6.txt")
+	for i := 0; i < len(line)-4; i++ {
+		set := runeSet(line[i : i+14])
+		if len(set) == 14 {
+			return i + 14
+		}
+	}
+	return -1
+}
